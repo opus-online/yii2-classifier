99334d1e1b41de9720f6f8b294961c7ac7f49d6c
Paris <ptheofan@gmail.com>
2015-11-12 11:13:05 +0100

    Importer - should be able to import directly from array

--- src/Importer.php
+++ src/Importer.php
@@ -34,17 +34,22 @@ class Importer
     }
 
     /**
-     * @param string $pathAlias
+     * @param array|string $pathAlias
      * @throws \Exception
      * @throws \yii\db\Exception
      */
-    public function import($pathAlias)
+    public function import($pathAliasOrArray)
     {
         $transaction = \Yii::$app->db->beginTransaction();
 
         try {
-            $filePath = \Yii::getAlias($pathAlias);
-            $conf = Yaml::parse($filePath);
+            if (is_string($pathAliasOrArray))
+            {
+                $filePath = \Yii::getAlias($pathAlias);
+                $conf = Yaml::parse($filePath);
+            }
+            elseif (is_array($pathAliasOrArray))
+                $conf = $pathAliasOrArray;
 
             foreach ($conf as $classifierCode => $classifierConf) {
                 $this->importClassifier(
